228023a7a0ee835ccc540ccc49b611b3b267d0e1
Gabriel Biankati <gabriel.2023014161@alunocesul.com>
2025-06-17 20:39:00 -0300

    feat: criado pasta users e users.repository

---
+++ src/users/users.repository.ts
@@ -0,0 +1,45 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+import { Prisma } from "@prisma/client";
+
+@Injectable()
+export class UsersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findManyRecent(): Promise<Prisma.UserUncheckedCreateInput[] | null> {
+    return await this.prisma.user.findMany();
+  }
+
+  async findById(id: string): Promise<Prisma.UserUncheckedCreateInput | null> {
+    return await this.prisma.user.findUnique({
+      where: {
+        id,
+      }
+    });
+  }
+
+  async save(data: Prisma.UserUncheckedCreateInput): Promise<void> {
+    await Promise.all([
+      this.prisma.user.update({
+        where: {
+          id: data.id?.toString(),
+        },
+        data,
+      }),
+    ]);
+  }
+
+  async create(product: Prisma.UserUncheckedCreateInput): Promise<void> {
+    await this.prisma.user.create({
+      data: product,
+    });
+  }
+
+  async delete(user: Prisma.UserUncheckedCreateInput): Promise<void> {
+    await this.prisma.user.delete({
+      where: {
+        id: user.id?.toString(),
+      }
+    });
+  }
+}
